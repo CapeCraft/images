843a95bb96c1e9e23b1773159e03313c36ee166c
James Harrison <james@fasttortoise.co.uk>
2020-08-28 15:25:21 +0100

    If DL_PATH is set it get auto downloaded at run

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ java -version
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+if [ -n "${DL_PATH}" ]; then
+curl -o ${SERVER_JARFILE} ${DL_PATH}
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
